6f6bbf55e69993c2b69ea8232749f36ba92ef419
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-11-23 15:47:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 //your JS code here. If required.
 
+const buttons = document.querySelectorAll('.btn');
+const stopButton = document.querySelector('.stop');
+
 let sounds = [];
 
 function playSound(e) {
   const soundName = e.target.getAttribute('data-sound');
-  const audio = new Audio(`sounds/${soundName}.mp3`);
+
+  const audio = document.createElement('audio');
+
+  audio.src = `sounds/${soundName}.mp3`;
+  document.body.appendChild(audio);
   audio.play();
+
   sounds.push(audio);
 }
 
-const stopButton = document.querySelector('.stop');
+buttons.forEach(button => {
+  button.addEventListener('click', playSound);
+});
 
 stopButton.addEventListener('click', () => {
   sounds.forEach((sound) => {
     sound.pause();
+    document.body.removeChild(sound);
   });
   sounds = [];
-});
+})
